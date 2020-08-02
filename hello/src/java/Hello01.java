56312edc8cb9f610300d0c5f32454e75415f8dcd
chuangjan <chuangjan@126.com>
2020-08-02 19:06:17 +0800

    Hello01

--- hello/src/java/Hello01.java
+++ hello/src/java/Hello01.java
@@ -4,10 +4,10 @@ public class Hello01 {
     public static void main(String[] args) {
         System.out.println("第二次测试git");
         System.out.println("第而次测试git");
-        System.out.println("第三次测试git");
-        System.out.println("第三次测试git");
-        System.out.println("第三次测试git");
-        System.out.println("第三次测试git");
+        System.out.println("第二次测试git");
+        System.out.println("第二次测试git");
+        System.out.println("第二次测试git");
+        System.out.println("第二次测试git");
 
         
         
