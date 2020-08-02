56d14eade25f8cabc9c8b12b20611031a7338821
chuangjan <54430662+chuangjan@users.noreply.github.com>
2020-08-02 18:55:53 +0800

    Update Hello01.java
    测试冲突

--- hello/src/java/Hello01.java
+++ hello/src/java/Hello01.java
@@ -4,10 +4,14 @@ public class Hello01 {
     public static void main(String[] args) {
         System.out.println("第一次测试git");
         System.out.println("第二次，之前全部忘完了");
-        System.out.println("第三次，之前全部忘完了");
-        System.out.println("第三次，之前全部忘完了");
-        System.out.println("第三次，之前全部忘完了");
-        System.out.println("第三次，之前全部忘完了");
+        System.out.println("第二次，之前全部忘完了");
+        System.out.println("第二次，之前全部忘完了");
+        System.out.println("第二次，之前全部忘完了");
+        System.out.println("第二次，之前全部忘完了");
+        System.out.println("第二次，之前全部忘完了");
+        
+        
+        
 
     }
 }
